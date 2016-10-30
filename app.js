77883928dae90428e789fdd080a83d3c2bfc4782
Abdel Haq Firdausy <abdel_31354@yahoo.co.id>
2016-10-30 18:44:08 +0700

    pug installed and tested

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 express=require('express')
+pug=require('pug')
 
 app=express()
+app.set('view engine','pug')
 
 app.get('/',function(req,res){
-	res.send('hello there')
+	//res.send('hello there')
+	res.render('index')
 })
 
 app.use(express.static('public'))
