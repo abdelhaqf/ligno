b45ca829f709b8ac87e1041e473927556daa7955
Abdel Haq Firdausy <abdel_31354@yahoo.co.id>
2016-10-30 18:50:54 +0700

    stylus installed and tested

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 express=require('express')
 pug=require('pug')
+stylus=require('express-stylus')
 
 app=express()
 app.set('view engine','pug')
 
 app.get('/',function(req,res){
-	//res.send('hello there')
 	res.render('index')
 })
 
+app.use(stylus({
+	src:'public'
+}))
 app.use(express.static('public'))
 
 app.listen(8080,function(){
