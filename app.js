b9b1b8990771185ffedc88a77b2e3ff2fa7df9b7
Abdel Haq Firdausy <abdel_31354@yahoo.co.id>
2016-10-30 19:10:26 +0700

    overtime.pug and route

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ bootstrap=require('bootstrap-styl')
 app=express()
 app.set('view engine','pug')
 
+//route start
 app.get('/',function(req,res){
 	res.render('index')
 })
+app.get('/overtime',function(req,res){
+	res.render('overtime')
+})
+//route end
 
 app.use(stylus({
 	src:'public',
